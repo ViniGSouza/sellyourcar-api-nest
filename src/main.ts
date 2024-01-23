8564d93ac214a96794542ae7de517f0124472b31
ViniGSouza <vinizintwitchtv@gmail.com>
2024-01-23 15:02:41 -0300

    add cors origin

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,6 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
   });
   await app.listen(3000);
 }
